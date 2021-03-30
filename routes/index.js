6f63c790b6f237c70d998f65ff5317c800cfded8
Azryel <AzryelShroud@gmail.com>
2021-03-30 20:20:50 +0200

    update with a new route for form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const member_controller = require ("../controllers/member");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get("/create_account", member_controller.member_create_get);
+router.post("/create_account", member_controller.member_create_post);
 
 module.exports = router;
