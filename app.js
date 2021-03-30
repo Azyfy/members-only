199e4323515cd2e3444bcf5b7fe606fafdd3aa07
Azryel <AzryelShroud@gmail.com>
2021-03-30 19:11:51 +0200

    connect to mongoDB

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+// DB
+const mongoose = require("mongoose");
+const mongoDB = process.env.MONGO_DB;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error;"));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
