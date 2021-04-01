7d6bbf7d8ddf2c573f5f4c85ee2c3674f4f54051
Azryel <AzryelShroud@gmail.com>
2021-04-01 19:16:38 +0200

    logout

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,10 @@ router.post("/login",  passport.authenticate("local", {
   failureRedirect: "/login"
 }));
 
+// logout
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
 module.exports = router;
