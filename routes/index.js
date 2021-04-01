3fc8fc1516d1d8d5f0eee8d3c2c481e88cf192d3
Azryel <AzryelShroud@gmail.com>
2021-04-01 19:08:49 +0200

    update route login

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+const passport = require("passport");
+
+
 const member_controller = require ("../controllers/member");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', user: req.user });
 });
 
 // create account get and post
 router.get("/create_account", member_controller.member_create_get);
 router.post("/create_account", member_controller.member_create_post);
+
+// login get and post
+router.get("/login", member_controller.member_login_get);
+router.post("/login",  passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login"
+}));
 
 module.exports = router;
