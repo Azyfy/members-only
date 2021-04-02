49d2c19c8f7facb7c81c8e1bcb0f8be900832030
Azryel <AzryelShroud@gmail.com>
2021-04-02 21:24:37 +0200

    check if loggedin and redirect from login and create account routes

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,22 @@ const passport = require("passport");
 
 const member_controller = require ("../controllers/member");
 
+function checkLoggedIn(req, res, next) {
+  if (!req.isAuthenticated()) { return next(); }
+  res.redirect('/clubhouse')
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 // create account get and post
-router.get("/create_account", member_controller.member_create_get);
+router.get("/create_account", checkLoggedIn, member_controller.member_create_get);
 router.post("/create_account", member_controller.member_create_post);
 
 // login get and post
-router.get("/login", member_controller.member_login_get);
+router.get("/login", checkLoggedIn, member_controller.member_login_get);
 router.post("/login",  passport.authenticate("local", {
   successRedirect: "/clubhouse",
   failureRedirect: "/login"
